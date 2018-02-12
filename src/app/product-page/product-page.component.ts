373eea997b0997cf33fcf9b9e661de5fcad004f8
fangwen zhao <frankzfw@umich.edu>
2018-02-12 15:27:53 -0700

    step5 fix

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../ProductService';
+import {ProductService} from '../product.service';
 import {Album} from '../album';
 @Component({
   selector: 'app-product-page',
