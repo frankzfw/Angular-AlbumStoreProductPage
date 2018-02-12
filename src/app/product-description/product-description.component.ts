e8c14a56d0cfdac5d74e06a0ac451b2040051d09
fangwen zhao <frankzfw@umich.edu>
2018-02-11 22:50:07 -0700

    step3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {ProductService} from '../product.service'
+import {ProductService} from '../product.service';
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo; 
+  albumInfo;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
